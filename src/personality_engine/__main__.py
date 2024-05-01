0a76c785dcd4a9389dfc50baa8d1ba5f51c336e6
Artem Tsaryuk <42806772+funcid@users.noreply.github.com>
2024-05-02 00:41:38 +0300

    add cors policy

--- src/personality_engine/__main__.py
+++ src/personality_engine/__main__.py
@@ -14,9 +14,22 @@ from .utils.torch import device as torch_device
 
 import pycuda.driver as cuda
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def initialize_handler() -> Callable[..., Any]:
     s3: S3Client = boto3.Session().client(
